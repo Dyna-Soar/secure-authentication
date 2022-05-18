c99a502d3d537793db539845696ed21d43319a29
Bodin <bodintiago@gmail.com>
2022-05-18 23:29:21 +0200

    endpoints now return json strings

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask
 
-from hashing import hash_password, create_hashed_password, check_hash
+from json import dumps
+
+from hashing import create_hashed_password, check_hash
 
 app = Flask(__name__)
 
@@ -8,10 +10,10 @@ app = Flask(__name__)
 @app.route("/request_authentication")
 def request_authentication(password, hash_sent):
     if check_hash(password, hash_sent):
-        return True
-    return False
+        return dumps({"IsPassword": True})
+    return dumps({"IsPassword": False})
 
 
 @app.route("/hash_new_password")
 def hash_new_password(password):
-    return create_hashed_password(password)
+    return dumps({"hashed_password": create_hashed_password(password)})
