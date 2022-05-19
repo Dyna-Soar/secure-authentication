8cb1d9dd2fec30dfccdc1faf28168f4eed31e7cc
Bodin <bodintiago@gmail.com>
2022-05-19 14:53:18 +0200

    add tests for auhtentication

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,29 +1,38 @@
 import pytest
 
-from main import app
-
-client = app.test_client()
-
 
 class TestRequestAuthentication:
-    def test_request_authentication(self):
-        pass
+    def test_request_invalid_authentication(self, fixture_client, fixture_users_passwords):
+        response = fixture_client.post("/request_authentication", data={
+            'password': fixture_users_passwords[1]["password"],
+            'hash_sent': fixture_users_passwords[1]["hashed_password"]
+        })
+        assert response.status_code == 200
+        assert b'false' in response.data
+
+    def test_request_valid_authentication(self, fixture_client, fixture_users_passwords):
+        response = fixture_client.post("/request_authentication", data={
+            'password': fixture_users_passwords[0]["password"],
+            'hash_sent': fixture_users_passwords[0]["hashed_password"]
+        })
+        assert response.status_code == 200
+        assert b'true' in response.data
 
 
 class TestRequestNewHash:
-    def test_password_less8char(self):
-        response = client.post("/hash_new_password", data={'password': "ascz"})
+    def test_password_less8char(self, fixture_client):
+        response = fixture_client.post("/hash_new_password", data={'password': "ascz"})
         assert response.status_code == 406
 
-    def test_password_nodigit(self):
-        response = client.post("/hash_new_password", data={'password': "asczhiflx"})
+    def test_password_nodigit(self, fixture_client):
+        response = fixture_client.post("/hash_new_password", data={'password': "asczhiflx"})
         assert response.status_code == 406
 
-    def test_password_nospechar(self):
-        response = client.post("/hash_new_password", data={'password': "asczhiflx8"})
+    def test_password_nospechar(self, fixture_client):
+        response = fixture_client.post("/hash_new_password", data={'password': "asczhiflx8"})
         assert response.status_code == 406
 
-    def test_password_valid(self):
-        response = client.post("/hash_new_password", data={'password': "asczhiflx.8"})
+    def test_password_valid(self, fixture_client):
+        response = fixture_client.post("/hash_new_password", data={'password': "asczhiflx.8"})
         assert response.status_code == 200
         assert b'hashed_password' in response.data
