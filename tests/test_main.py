2d7c6ddc2cfb43a594e999979c73b1a13ee035a3
Bodin <bodintiago@gmail.com>
2022-05-20 15:04:51 +0200

    add tests for post requests with missing keys in body

--- tests/test_main.py
+++ tests/test_main.py
@@ -18,6 +18,20 @@ class TestRequestAuthentication:
         assert response.status_code == 200
         assert b'true' in response.data
 
+    def test_no_password_authentication(self, fixture_client, fixture_users_passwords):
+        response = fixture_client.post("/request_authentication", data={
+            'hash_sent': fixture_users_passwords[0]["hashed_password"]
+        })
+        assert response.status_code == 400
+        assert b'Missing Data' in response.data
+
+    def test_no_hash_authentication(self, fixture_client, fixture_users_passwords):
+        response = fixture_client.post("/request_authentication", data={
+            'password': fixture_users_passwords[0]["password"],
+        })
+        assert response.status_code == 400
+        assert b'Missing Data' in response.data
+
 
 class TestRequestNewHash:
     def test_password_less8char(self, fixture_client):
@@ -36,3 +50,8 @@ class TestRequestNewHash:
         response = fixture_client.post("/hash_new_password", data={'password': "asczhiflx.8"})
         assert response.status_code == 200
         assert b'hashed_password' in response.data
+
+    def test_no_password(self, fixture_client):
+        response = fixture_client.post("/hash_new_password")
+        assert response.status_code == 400
+        assert b'Missing Data' in response.data
