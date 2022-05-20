ea8409186ad86ef2f1c628b81b888f0d3a19ca62
Bodin <bodintiago@gmail.com>
2022-05-20 15:05:26 +0200

    handling post request with missing keys in body

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 from hashing import create_hashed_password, check_hash
 
@@ -7,17 +7,23 @@ app = Flask(__name__)
 
 @app.route("/request_authentication", methods=['POST'])
 def request_authentication():
+    if request.form.get("password") is None:
+        return jsonify({"Missing Data": "No password provided"}), 400
+    if request.form.get("hash_sent") is None:
+        return jsonify({"Missing Data": "No hash provided"}), 400
     password = request.form["password"]
     hash_sent = request.form["hash_sent"]
     if check_hash(password, hash_sent):
-        return {"IsPassword": True}, 200
-    return {"IsPassword": False}, 200
+        return jsonify({"IsPassword": True}), 200
+    return jsonify({"IsPassword": False}), 200
 
 
 @app.route("/hash_new_password", methods=['POST'])
 def hash_new_password():
+    if request.form.get("password") is None:
+        return jsonify({"Missing Data": "No password provided"}), 400
     password = request.form["password"]
     hashed_password = create_hashed_password(password)
     if hashed_password is False:
-        return {"Error Message": "Password not valid, should contain at least 8 characters; 1 digit; 1 special character"}, 406
-    return {"hashed_password": hashed_password}, 200
+        return jsonify({"Error Message": "Password not valid, should contain at least 8 characters; 1 digit; 1 special character"}), 406
+    return jsonify({"hashed_password": hashed_password}), 200
